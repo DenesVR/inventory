2dcf8178e7a0a38d032cd156ca1e3917546d2d08
DenesVR <denesvanroy@gmail.com>
2021-06-14 14:57:31 +0200

    Login & Register

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  InputRightElement,
+  InputGroup,
+} from '@chakra-ui/react';
 
 function Login() {
-  return <div></div>;
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
+  return (
+    <form>
+      <FormControl id="email-login">
+        <FormLabel>Email</FormLabel>
+        <Input type="email" placeholder="Email" />
+      </FormControl>
+      <FormControl id="ww-login">
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input type={show ? 'text' : 'password'} placeholder="Password" />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? 'Hide' : 'Show'}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <Button mt={4} colorScheme="teal" type="submit">
+        Login
+      </Button>
+    </form>
+  );
 }
 
 export default Login;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  InputRightElement,
+  InputGroup,
+} from '@chakra-ui/react';
 
 function Register() {
-  return <div></div>;
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
+  return (
+    <form>
+      <FormControl id="voornaam" isRequired>
+        <FormLabel>Voornaam</FormLabel>
+        <Input placeholder="Voornaam" />
+      </FormControl>
+      <FormControl id="achternaam" isRequired>
+        <FormLabel>Achternaam</FormLabel>
+        <Input placeholder="Achternaam" />
+      </FormControl>
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input placeholder="Email" />
+      </FormControl>
+      <FormControl id="Password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input type={show ? 'text' : 'password'} placeholder="Password" />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? 'Hide' : 'Show'}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <Button mt={4} colorScheme="teal" type="submit">
+        Register
+      </Button>
+    </form>
+  );
 }
 
 export default Register;
